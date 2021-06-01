b7f4b9789e1594fbce160938ed5e3e5f6b45ecff
gschmutz <guido.schmutz@trivadis.com>
2021-06-01 08:35:19 +0200

    add dotnet solution

---
+++ 05-producing-consuming-kafka-with-dotnet/src/consumer/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Threading;
+using Confluent.Kafka;
+
+namespace consumer
+{
+    class Program
+    {
+        const string brokerList = "dataplatform:9092,dataplatform:9093";
+        const string topicName = "test-dotnet-topic";
+        const string groupId = "KakfaConsumerAuto";
+
+        static void Main(string[] args)
+        {
+            runConsumerManual();
+        }
+        static void runConsumerAuto()
+        {
+            var config = new ConsumerConfig
+            {
+                BootstrapServers = brokerList,
+                GroupId = groupId,
+                AutoOffsetReset = AutoOffsetReset.Earliest,
+                EnableAutoCommit = true,
+                AutoCommitIntervalMs = 10000
+            };
+
+            bool cancelled = false;
+
+            using (var consumer = new ConsumerBuilder<Ignore, string>(config).Build())
+            {
+                consumer.Subscribe(topicName);
+                var cancelToken = new CancellationTokenSource();
+
+                while (!cancelled) {
+                    var consumeResult = consumer.Consume(cancelToken.Token);
+                    // handle message
+                    Console.WriteLine($"Consumer Record:(Key: {consumeResult.Message.Key}, Value: {consumeResult.Message.Value} Partition: {consumeResult.TopicPartition.Partition} Offset: {consumeResult.TopicPartitionOffset.Offset}");
+                }
+                consumer.Close();
+            }
+        }   
+        static void runConsumerManual()
+        {
+            var config = new ConsumerConfig
+            {
+                BootstrapServers = brokerList,
+                GroupId = groupId,
+                EnableAutoCommit = false
+            };
+
+            bool cancelled = false;         
+            int noRecordsCount = 0;
+
+            using (var consumer = new ConsumerBuilder<Ignore, string>(config).Build())
+            {
+                consumer.Subscribe(topicName);
+                var cancelToken = new CancellationTokenSource();
+                ConsumeResult<Ignore, string> consumeResult = null;
+
+                while (!cancelled) {
+                    consumeResult = consumer.Consume(cancelToken.Token);
+                    noRecordsCount++;
+                    
+                    // handle message
+                    Console.WriteLine($"Consumer Record:(Key: {consumeResult.Message.Key}, Value: {consumeResult.Message.Value} Partition: {consumeResult.TopicPartition.Partition} Offset: {consumeResult.TopicPartitionOffset.Offset}");
+
+                    if (consumeResult.Offset % 50 == 0) {
+                        consumer.Commit(consumeResult);
+                    }
+
+                }
+                
+                // commit the rest
+                consumer.Commit(consumeResult);
+
+                consumer.Close();
+            }
+        }  
+
+    }     
+}
---
+++ 05-producing-consuming-kafka-with-dotnet/src/producer/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Threading;
+using Confluent.Kafka;
+
+namespace producer
+{
+    class Program
+    {
+        const string brokerList = "dataplatform:9092,dataplatform:9093";
+        const string topicName = "test-dotnet-topic";
+
+        static void Main(string[] args)
+        {
+            if (args.Length == 0) {
+                runProducerWithKey(100, 10, 0);
+            } else {
+                runProducerWithKey(int.Parse(args[0]), int.Parse(args[1]), int.Parse(args[2]));
+            }
+        }
+
+        static void runProducer(int totalMessages, int waitMsInBetween, int id)
+        {
+            var config = new ProducerConfig { BootstrapServers = brokerList };
+
+            // Create the Kafka Producer
+            using (var producer = new ProducerBuilder<Null, string>(config).Build())
+            {
+                for (int index = 0; index < totalMessages; index++)
+                {
+                    long time = DateTimeOffset.Now.ToUnixTimeMilliseconds();
+
+                    // Construct the message value
+                    string value = "[" + index + ":" + id + "] Hello Kafka " + DateTimeOffset.Now;
+                    
+                    try
+                        {
+                            // send the message to Kafka
+                            var task =  producer.ProduceAsync(topicName, new Message<Null, string> { Value = value});    
+                            if (task.IsFaulted) {
+                                
+                            } else {
+                                long elapsedTime = DateTimeOffset.Now.ToUnixTimeMilliseconds() - time;
+
+                                Console.WriteLine($"[{id}] sent record (key={task.Result.Key} value={task.Result.Value}) meta (partition={task.Result.TopicPartition.Partition}, offset={task.Result.TopicPartitionOffset.Offset}, time={elapsedTime})");
+                            }
+                        }
+                        catch (ProduceException<string, string> e)
+                        {
+                            Console.WriteLine($"failed to deliver message: {e.Message} [{e.Error.Code}]");
+                        }
+
+                        Thread.Sleep(waitMsInBetween);
+                }
+           
+            }  
+        }
+
+        static void runProducerWithKey(int totalMessages, int waitMsInBetween, int id)
+        {
+            var config = new ProducerConfig { BootstrapServers = brokerList };
+
+            // Create the Kafka Producer
+            using (var producer = new ProducerBuilder<long, string>(config).Build())
+            {
+                for (int index = 0; index < totalMessages; index++)
+                {
+                    long time = DateTimeOffset.Now.ToUnixTimeMilliseconds();
+
+                    // Construct the message value
+                    string value = "[" + index + ":" + id + "] Hello Kafka " + DateTimeOffset.Now;
+                    
+                    try
+                        {
+                            // send the message to Kafka
+                            var task =  producer.ProduceAsync(topicName, new Message<long, string> { Key = id, Value = value});    
+                            if (task.IsFaulted) {
+                                
+                            } else {
+                                long elapsedTime = DateTimeOffset.Now.ToUnixTimeMilliseconds() - time;
+
+                                Console.WriteLine($"[{id}] sent record (key={task.Result.Key} value={task.Result.Value}) meta (partition={task.Result.TopicPartition.Partition}, offset={task.Result.TopicPartitionOffset.Offset}, time={elapsedTime})");
+                            }
+                        }
+                        catch (ProduceException<string, string> e)
+                        {
+                            Console.WriteLine($"failed to deliver message: {e.Message} [{e.Error.Code}]");
+                        }
+
+                        Thread.Sleep(waitMsInBetween);
+                }
+           
+            }  
+        }
+
+    }
+}
