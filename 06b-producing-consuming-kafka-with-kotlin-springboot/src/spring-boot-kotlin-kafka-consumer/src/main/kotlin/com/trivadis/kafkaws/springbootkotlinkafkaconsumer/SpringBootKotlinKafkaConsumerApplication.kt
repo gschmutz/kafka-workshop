f17368460781ea41ca22e66c30b106fe05a13531
Guido Schmutz <guido.schmutz@trivadis.com>
2022-06-05 12:40:47 +0200

    update

--- 06b-producing-consuming-kafka-with-kotlin-springboot/src/spring-boot-kotlin-kafka-consumer/src/main/kotlin/com/trivadis/kafkaws/springbootkotlinkafkaconsumer/SpringBootKotlinKafkaConsumerApplication.kt
+++ 06b-producing-consuming-kafka-with-kotlin-springboot/src/spring-boot-kotlin-kafka-consumer/src/main/kotlin/com/trivadis/kafkaws/springbootkotlinkafkaconsumer/SpringBootKotlinKafkaConsumerApplication.kt
@@ -1,12 +1,7 @@
 package com.trivadis.kafkaws.springbootkotlinkafkaconsumer
 
-import org.apache.kafka.clients.consumer.ConsumerRecord
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.context.annotation.Bean
-import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory
-import org.springframework.kafka.core.ConsumerFactory
 
 
 @SpringBootApplication
