fb49f7ee22bd78abb4bb77c223ffe7a2d9dc490b
Guido Schmutz <guido.schmutz@trivadis.com>
2023-01-24 18:33:27 +0100

    Create KafkaProducerSyncWithHeader.java

---
+++ 04-producing-consuming-kafka-with-java/src/java-kafka/src/main/java/com/trivadis/kafkaws/producer/KafkaProducerSyncWithHeader.java
@@ -0,0 +1,68 @@
+package com.trivadis.kafkaws.producer;
+
+import org.apache.kafka.clients.producer.*;
+import org.apache.kafka.common.header.Header;
+import org.apache.kafka.common.header.internals.RecordHeader;
+import org.apache.kafka.common.serialization.LongSerializer;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.nio.charset.StandardCharsets;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+public class KafkaProducerSyncWithHeader {
+
+    private final static String TOPIC = "test-java-topic";
+    private final static String BOOTSTRAP_SERVERS
+            = "dataplatform:9092,dataplatform:9093";
+
+    private static Producer<Long, String> createProducer() {
+        Properties props = new Properties();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS);
+        props.put(ProducerConfig.CLIENT_ID_CONFIG, "KafkaExampleProducer");
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,
+                LongSerializer.class.getName());
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,
+                StringSerializer.class.getName());
+        return new KafkaProducer<>(props);
+    }
+
+    private static void runProducer(int sendMessageCount, int waitMsInBetween, long id) throws Exception {
+        Long key = (id > 0) ? id : null;
+
+        try (Producer<Long, String> producer = createProducer()) {
+            for (int index = 0; index < sendMessageCount; index++) {
+                long time = System.currentTimeMillis();
+                Integer partition = null;
+                Long timestamp = null;
+                List<Header> headers = new ArrayList<>();
+                headers.add(new RecordHeader("String", "string".getBytes(StandardCharsets.UTF_8)));
+                headers.add(new RecordHeader("Long", Long.valueOf("0").getBytes(StandardCharsets.UTF_8)));
+
+                ProducerRecord<Long, String> record = new ProducerRecord<>(TOPIC, partition,  timestamp, key, "[" + id + "] Hello Kafka " + index + " => " + LocalDateTime.now(), headers);
+                
+                RecordMetadata metadata = producer.send(record).get();
+
+                long elapsedTime = System.currentTimeMillis() - time;
+                System.out.printf("[" + id + "] sent record(key=%s value=%s) "
+                        + "meta(partition=%d, offset=%d) time=%d\n",
+                        record.key(), record.value(), metadata.partition(),
+                        metadata.offset(), elapsedTime);
+
+                // Simulate slow processing
+                Thread.sleep(waitMsInBetween);
+            }
+        }
+    }
+
+    public static void main(String... args) throws Exception {
+        if (args.length == 0) {
+            runProducer(100, 10, 0);
+        } else {
+            runProducer(Integer.parseInt(args[0]), Integer.parseInt(args[1]), Long.parseLong(args[2]));
+        }
+    }
+
+}
