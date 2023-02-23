2979a880184d6f791cb6902deccb86eeb0bc69c0
Guido Schmutz <guido.schmutz@trivadis.com>
2023-02-23 13:58:34 +0100

    session window testing

--- 12-using-kafka-streams-simple/src/kafka-streams-simple/src/test/java/com/trivadis/kafkaws/kstream/countsession/KafkaStreamsRunnerCountSessionWindowedSuppressedDSLTest.java
+++ 12-using-kafka-streams-simple/src/kafka-streams-simple/src/test/java/com/trivadis/kafkaws/kstream/countsession/KafkaStreamsRunnerCountSessionWindowedSuppressedDSLTest.java
@@ -25,7 +25,8 @@ public class KafkaStreamsRunnerCountSessionWindowedSuppressedDSLTest {
 
     private Properties props = KafkaStreamsRunnerCountSessionWindowedSuppressedDSL.getKafkaProperties();;
 
-    private final String KEY = "A";
+    private final String KEY_A = "A";
+    private final String KEY_B = "B";
     private final Instant TS_09_00_00 = Instant.parse("2022-02-22T09:00:00.000Z");
     private final Instant TS_09_00_15 = Instant.parse("2022-02-22T09:00:15.000Z");
     private final Instant TS_09_00_16 = Instant.parse("2022-02-22T09:00:16.000Z");
@@ -45,6 +46,8 @@ public class KafkaStreamsRunnerCountSessionWindowedSuppressedDSLTest {
         //this.testTopology.stop();
     }
 
+
+    // 10
     @Test
     void test30SecGapWithNoGracePeriod_AllInOrder() {
         // test a session window with an inactivity gap to 30 seconds and 15 seconds grace period
@@ -54,15 +57,15 @@ public class KafkaStreamsRunnerCountSessionWindowedSuppressedDSLTest {
         outputTopic = testDriver.createOutputTopic("test-kstream-output-topic", Serdes.String().deserializer(), Serdes.String().deserializer());
 
         // start at 09:00:00
-        inputTopic.pipeInput(KEY, "cat", TS_09_00_00.toEpochMilli());
+        inputTopic.pipeInput(KEY_A, "cat", TS_09_00_00.toEpochMilli());
         // still within inactivity gap
-        inputTopic.pipeInput(KEY, "dog", TS_09_00_15.toEpochMilli());
+        inputTopic.pipeInput(KEY_A, "dog", TS_09_00_15.toEpochMilli());
         // after activity gap so window will close with 09:00:15 and new one starts with 09:00:46
-        inputTopic.pipeInput(KEY, "foo", TS_09_00_46.toEpochMilli());
+        inputTopic.pipeInput(KEY_A, "foo", TS_09_00_46.toEpochMilli());
         // still within inactivity gap
-        inputTopic.pipeInput(KEY, "foo", TS_09_00_47.toEpochMilli());
+        inputTopic.pipeInput(KEY_A, "foo", TS_09_00_47.toEpochMilli());
         // end previous session window
-        inputTopic.pipeInput(KEY, "foo", TS_END.toEpochMilli());
+        inputTopic.pipeInput(KEY_A, "foo", TS_END.toEpochMilli());
 
         List<TestRecord<String, String>> outRecords =
                 outputTopic.readRecordsToList();
@@ -75,25 +78,25 @@ public class KafkaStreamsRunnerCountSessionWindowedSuppressedDSLTest {
         System.out.println(outRecords.get(1));
     }
 
-
+    // 15
     @Test
     void test30SecGapWithNoGracePeriod_WithLateArrival() {
-        // test a session window with an inactivity gap to 30 seconds and 15 seconds grace period
+        // test a session window with an inactivity gap to 30 seconds and 0 seconds grace period
         Topology topology = KafkaStreamsRunnerCountSessionWindowedSuppressedDSL.getTopology(30,0);
         testDriver = new TopologyTestDriver(topology, props);
         inputTopic = testDriver.createInputTopic("test-kstream-input-topic", Serdes.String().serializer(), Serdes.String().serializer());
         outputTopic = testDriver.createOutputTopic("test-kstream-output-topic", Serdes.String().deserializer(), Serdes.String().deserializer());
 
         // start at 09:00:00
-        inputTopic.pipeInput(KEY, "cat", TS_09_00_00.toEpochMilli());
+        inputTopic.pipeInput(KEY_A, "cat", TS_09_00_00.toEpochMilli());
         // still within inactivity gap
-        inputTopic.pipeInput(KEY, "dog", TS_09_00_15.toEpochMilli());
+        inputTopic.pipeInput(KEY_A, "dog", TS_09_00_15.toEpochMilli());
         // after activity gap so window will close with 09:00:15 and new one starts with 09:00:47
-        inputTopic.pipeInput(KEY, "foo", TS_09_00_47.toEpochMilli());
+        inputTopic.pipeInput(KEY_A, "foo", TS_09_00_47.toEpochMilli());
         // this is ignored with 09:00:16 as it is after the window has been closed and there is no grace period
-        inputTopic.pipeInput(KEY, "foo", TS_09_00_16.toEpochMilli());
+        inputTopic.pipeInput(KEY_A, "foo", TS_09_00_16.toEpochMilli());
         // end previous session window
-        inputTopic.pipeInput(KEY, "foo", TS_END.toEpochMilli());
+        inputTopic.pipeInput(KEY_A, "foo", TS_END.toEpochMilli());
 
         List<TestRecord<String, String>> outRecords =
                 outputTopic.readRecordsToList();
@@ -106,24 +109,56 @@ public class KafkaStreamsRunnerCountSessionWindowedSuppressedDSLTest {
         System.out.println(outRecords.get(1));
     }
 
+    // 20
+    @Test
+    void test30SecGapWithNoGracePeriod_WithSecondKey() {
+        // test a session window with an inactivity gap to 30 seconds and 0 seconds grace period
+        Topology topology = KafkaStreamsRunnerCountSessionWindowedSuppressedDSL.getTopology(30,0);
+        testDriver = new TopologyTestDriver(topology, props);
+        inputTopic = testDriver.createInputTopic("test-kstream-input-topic", Serdes.String().serializer(), Serdes.String().serializer());
+        outputTopic = testDriver.createOutputTopic("test-kstream-output-topic", Serdes.String().deserializer(), Serdes.String().deserializer());
+
+        // start at 09:00:00
+        inputTopic.pipeInput(KEY_A, "cat", TS_09_00_00.toEpochMilli());
+        // still within inactivity gap
+        inputTopic.pipeInput(KEY_A, "dog", TS_09_00_15.toEpochMilli());
+        // after activity gap with other KEY so window will close with 09:00:15 and new one starts with 09:00:47
+        inputTopic.pipeInput(KEY_B, "foo", TS_09_00_47.toEpochMilli());
+        // this is ignored with 09:00:16 as it is after the window has been closed and there is no grace period
+        inputTopic.pipeInput(KEY_B, "foo", TS_09_00_48.toEpochMilli());
+        // end previous session window
+        inputTopic.pipeInput(KEY_A, "foo", TS_END.toEpochMilli());
+
+        List<TestRecord<String, String>> outRecords =
+                outputTopic.readRecordsToList();
+        // two session windows
+        assertThat(outRecords).hasSize(2);
+        assertEquals(outRecords.get(0).value(), "2");
+        assertEquals(outRecords.get(1).value(), "2");
+
+        System.out.println(outRecords.get(0));
+        System.out.println(outRecords.get(1));
+    }
+
+    // 25
     @Test
     void test30SecGapWithNoGracePeriod_WithLateArrivalSameAsWindowEnd() {
-        // test a session window with an inactivity gap to 30 seconds and 15 seconds grace period
+        // test a session window with an inactivity gap to 30 seconds and 0 seconds grace period
         Topology topology = KafkaStreamsRunnerCountSessionWindowedSuppressedDSL.getTopology(30,0);
         testDriver = new TopologyTestDriver(topology, props);
         inputTopic = testDriver.createInputTopic("test-kstream-input-topic", Serdes.String().serializer(), Serdes.String().serializer());
         outputTopic = testDriver.createOutputTopic("test-kstream-output-topic", Serdes.String().deserializer(), Serdes.String().deserializer());
 
         // start at 09:00:00
-        inputTopic.pipeInput(KEY, "cat", TS_09_00_00.toEpochMilli());
+        inputTopic.pipeInput(KEY_A, "cat", TS_09_00_00.toEpochMilli());
         // still within inactivity gap
-        inputTopic.pipeInput(KEY, "dog", TS_09_00_15.toEpochMilli());
+        inputTopic.pipeInput(KEY_A, "dog", TS_09_00_15.toEpochMilli());
         // after activity gap so window will close with 09:00:15 and new one starts with 09:00:47
-        inputTopic.pipeInput(KEY, "foo", TS_09_00_47.toEpochMilli());
+        inputTopic.pipeInput(KEY_A, "foo", TS_09_00_47.toEpochMilli());
         // this is ignored with 09:00:15 as it is after the window has been closed and there is no grace period
-        inputTopic.pipeInput(KEY, "foo", TS_09_00_15.toEpochMilli());
+        inputTopic.pipeInput(KEY_A, "foo", TS_09_00_15.toEpochMilli());
         // end previous session window
-        inputTopic.pipeInput(KEY, "foo", TS_END.toEpochMilli());
+        inputTopic.pipeInput(KEY_A, "foo", TS_END.toEpochMilli());
 
         List<TestRecord<String, String>> outRecords =
                 outputTopic.readRecordsToList();
@@ -136,6 +171,7 @@ public class KafkaStreamsRunnerCountSessionWindowedSuppressedDSLTest {
         System.out.println(outRecords.get(1));
     }
 
+    // 30
     @Test
     void test30SecGapWith15SecGracePeriod_WithLateArrival() {
         // test a session window with an inactivity gap to 30 seconds and 15 seconds grace period
@@ -145,15 +181,15 @@ public class KafkaStreamsRunnerCountSessionWindowedSuppressedDSLTest {
         outputTopic = testDriver.createOutputTopic("test-kstream-output-topic", Serdes.String().deserializer(), Serdes.String().deserializer());
 
         // start at 09:00:00
-        inputTopic.pipeInput(KEY, "cat", TS_09_00_00.toEpochMilli());
+        inputTopic.pipeInput(KEY_A, "cat", TS_09_00_00.toEpochMilli());
         // still within inactivity gap
-        inputTopic.pipeInput(KEY, "dog", TS_09_00_15.toEpochMilli());
+        inputTopic.pipeInput(KEY_A, "dog", TS_09_00_15.toEpochMilli());
         // after activity gap so window will close with 09:00:15 and new one starts with 09:00:47
-        inputTopic.pipeInput(KEY, "foo", TS_09_00_47.toEpochMilli());
+        inputTopic.pipeInput(KEY_A, "foo", TS_09_00_47.toEpochMilli());
         // this is still within grace period (window was closed at 09:00:15, by that window end is extended to 09:00:47 (9:00:17 + 30s) and previous event included in the session
-        inputTopic.pipeInput(KEY, "foo", TS_09_00_17.toEpochMilli());
+        inputTopic.pipeInput(KEY_A, "foo", TS_09_00_17.toEpochMilli());
         // end previous session window
-        inputTopic.pipeInput(KEY, "foo", TS_END.toEpochMilli());
+        inputTopic.pipeInput(KEY_A, "foo", TS_END.toEpochMilli());
 
         List<TestRecord<String, String>> outRecords =
                 outputTopic.readRecordsToList();
@@ -164,6 +200,7 @@ public class KafkaStreamsRunnerCountSessionWindowedSuppressedDSLTest {
         System.out.println(outRecords.get(0));
     }
 
+    // 35
     @Test
     void test30SecGapWith15SecGracePeriod_WithLateArrivalSameAsEndOfWindow() {
         // test a session window with an inactivity gap to 30 seconds and 15 seconds grace period
@@ -173,15 +210,15 @@ public class KafkaStreamsRunnerCountSessionWindowedSuppressedDSLTest {
         outputTopic = testDriver.createOutputTopic("test-kstream-output-topic", Serdes.String().deserializer(), Serdes.String().deserializer());
 
         // start at 09:00:00
-        inputTopic.pipeInput(KEY, "cat", TS_09_00_00.toEpochMilli());
+        inputTopic.pipeInput(KEY_A, "cat", TS_09_00_00.toEpochMilli());
         // still within inactivity gap
-        inputTopic.pipeInput(KEY, "dog", TS_09_00_15.toEpochMilli());
+        inputTopic.pipeInput(KEY_A, "dog", TS_09_00_15.toEpochMilli());
         // after activity gap so window will close with 09:00:15 and new one starts with 09:00:47
-        inputTopic.pipeInput(KEY, "foo", TS_09_00_47.toEpochMilli());
+        inputTopic.pipeInput(KEY_A, "foo", TS_09_00_47.toEpochMilli());
         // this is still within grace period (window was closed at 09:00:15, by that window end is not extended and stays at 09:00:45 (9:00:15 + 30s)
-        inputTopic.pipeInput(KEY, "foo", TS_09_00_15.toEpochMilli());
+        inputTopic.pipeInput(KEY_A, "foo", TS_09_00_15.toEpochMilli());
         // end previous session window
-        inputTopic.pipeInput(KEY, "foo", TS_END.toEpochMilli());
+        inputTopic.pipeInput(KEY_A, "foo", TS_END.toEpochMilli());
 
         List<TestRecord<String, String>> outRecords =
                 outputTopic.readRecordsToList();
