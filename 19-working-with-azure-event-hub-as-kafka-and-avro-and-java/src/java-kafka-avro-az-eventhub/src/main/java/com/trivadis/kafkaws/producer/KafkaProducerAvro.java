ef6bf5948d740951b58ad155a652c092c70f1d4a
Guido Schmutz <guido.schmutz@trivadis.com>
2023-08-09 07:54:19 +0200

    add azure event hub as kafka

---
+++ 19-working-with-azure-event-hub-as-kafka-and-avro-and-java/src/java-kafka-avro-az-eventhub/src/main/java/com/trivadis/kafkaws/producer/KafkaProducerAvro.java
@@ -0,0 +1,65 @@
+package com.trivadis.kafkaws.producer;
+
+import com.trivadis.kafkaws.avro.v1.Notification;
+import io.confluent.kafka.serializers.KafkaAvroSerializer;
+import io.confluent.kafka.serializers.KafkaAvroSerializerConfig;
+import org.apache.kafka.clients.producer.*;
+
+import java.util.Properties;
+
+public class KafkaProducerAvro {
+
+    private final static String TOPIC = "test-java-avro-topic";
+    private final static String BOOTSTRAP_SERVERS =
+            "dataplatform:9092, dataplatform:9093, dataplatform:9094";
+    private final static String SCHEMA_REGISTRY_URL = "http://dataplatform:8081";
+
+    private static Producer<Long, Notification> createProducer() {
+        Properties props = new Properties();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS);
+        props.put(ProducerConfig.CLIENT_ID_CONFIG, "KafkaExampleProducer");
+        //props.put(KafkaAvroSerializerConfig.AUTO_REGISTER_SCHEMAS, "false");
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, KafkaAvroSerializer.class.getName());
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, KafkaAvroSerializer.class.getName());
+        props.put(KafkaAvroSerializerConfig.SCHEMA_REGISTRY_URL_CONFIG, SCHEMA_REGISTRY_URL);   // use constant for "schema.registry.url"
+
+        return new KafkaProducer<>(props);
+    }
+
+    static void runProducer(final int sendMessageCount, final int waitMsInBetween, final long id) throws Exception {
+        final Producer<Long, Notification> producer = createProducer();
+        long time = System.currentTimeMillis();
+        Long key = (id > 0) ? id : null;
+
+        try {
+            for (long index = 0; index < sendMessageCount; index++) {
+                Notification notification = new Notification(id, "Hello Kafka " + index);
+
+                final ProducerRecord<Long, Notification> record =
+                        new ProducerRecord<>(TOPIC, key, notification);
+
+                RecordMetadata metadata = producer.send(record).get();
+
+                long elapsedTime = System.currentTimeMillis() - time;
+                System.out.printf("[" + id + "] sent record(key=%s value=%s) " +
+                                "meta(partition=%d, offset=%d) time=%d\n",
+                        record.key(), record.value(), metadata.partition(),
+                        metadata.offset(), elapsedTime);
+                time = System.currentTimeMillis();
+
+                Thread.sleep(waitMsInBetween);
+            }
+        } finally {
+            producer.flush();
+            producer.close();
+        }
+    }
+
+    public static void main(String... args) throws Exception {
+        if (args.length == 0) {
+            runProducer(100,10,0);
+        } else {
+            runProducer(Integer.parseInt(args[0]),Integer.parseInt(args[1]),Long.parseLong(args[2]));
+        }
+    }
+}
